e2e0bf1aa913360994b88c0529d8f2da7e8577b9
Frederic Robert <fredrob622@free.com>
2023-03-03 23:02:18 +0100

    1ier commit de funigo (backend/frontend)
    funigo (backend/frontend) 03/03/2023

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// importer l'application app.js
+const app = require("./app");
+
+// Importer le package pour utiliser les variables d'environnement
+const dotenv = require("dotenv");
+// La méthode config charge les variables dans le fichier process.env.
+const result = dotenv.config();
+
+const express = require('express');
+
+// configurer le port avec la methode set de express. Attribut la valeur process.env.PORT (3000) à port
+app.set("port", process.env.PORT);
+
+//  URL http://localhost:3000/ renvoie   "Hello world"
+app.get('/', (req, res) => {
+  res.send(`Hello World! vous êtes sur localhost et le port ${process.env.PORT} => URL http://localhost:3000/ !`)
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Express Application exemple à l'écoute sur le port ${process.env.PORT}!`)
+});
+
+
